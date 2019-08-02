5db280ec998872adc31f4a9e6574976fde6f270d
stojanovskitrajce <53583292+stojanovskitrajce@users.noreply.github.com>
2019-08-02 17:06:43 +0200

    kalkulator 1

---
+++ src/math.kt
@@ -0,0 +1,9 @@
+fun main(args:Array<String>){
+    //simple calc
+    println("Vnesi go prviot broj:  ")
+    var n1:int= readLine()!!.toInt()
+    println("vnesi go vtoriot borj: ")
+    var n2:Int= readLine()!!.toInt()
+    var sum:Float=n1+n2
+    println(sum)
+}

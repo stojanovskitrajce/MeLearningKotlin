09a5ea9a273d4148434cb7f85fc11d2d735de509
stojanovskitrajce <53583292+stojanovskitrajce@users.noreply.github.com>
2019-08-03 09:02:24 +0200

    if else

---
+++ src/ifElse.kt
@@ -0,0 +1,10 @@
+fun main (args:Array<String>){
+    println("koliko godina imas: ")
+    var age:Int = readLine()!!.toInt()
+    if(age>18){
+        println("auu golemo mugare")
+    }
+    else {
+        println("malenko si ushte!!!")
+    }
+}
